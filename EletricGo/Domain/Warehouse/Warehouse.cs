f84f0a0321cc85f96374d9f766ba9273e428156d
NunoBarbosa20 <1210822@isep.ipp.pt>
2022-10-17 10:04:03 +0100

    #4 Warehouse.cd and DTO bug fixes

--- EletricGo/Domain/Warehouse/Warehouse.cs
+++ EletricGo/Domain/Warehouse/Warehouse.cs
@@ -6,28 +6,32 @@ using System.Threading.Tasks;
 using EletricGo.Domain.Shared;
 using EletricGo.Domain.Warehouse;
 
-namespace EletricGo.Domain.Products
+namespace EletricGo.Domain.Warehouse
 {
-    public class Warehouse : Entity<WarehouseID>, IAggregate {
+    public class Warehouse : Entity<WarehouseID> {
         public WarehouseID warehouseID { get; private set; }
+
+        public WarehouseAddress warehouseAddress { get; private set;}
         
         public WarehouseDesignation warehouseDesignation { get; private set; }
         
-        public Warehouse(WarehouseID warehouseID, WarehouseDesignation warehouseDesignation)
+        public Warehouse(WarehouseID warehouseID,WarehouseAddress warehouseAddress, WarehouseDesignation warehouseDesignation)
         {
             this.warehouseID = warehouseID;
+            this.warehouseAddress = warehouseAddress;
             this.warehouseDesignation = warehouseDesignation;
         }
         
         public Warehouse(WarehouseDTO warehouseDTO)
         {
             this.warehouseID = new WarehouseID(warehouseDTO.warehouseID);
-            this.warehouseDesignation = new WarehouseDesignation(warehouseDTO.warehouseDesignation);
+            this.warehouseAddress = new WarehouseAddress(warehouseDTO.address);
+            this.warehouseDesignation = new WarehouseDesignation(warehouseDTO.designation);
         }
         
         public WarehouseDTO toWarehouseDTO()
         {
-            return new WarehouseDTO(this.warehouseID.AsGuid(), this.warehouseDesignation.Value);
+            return new WarehouseDTO(this.warehouseID.AsGuid(), this.warehouseAddress.Value,this.warehouseDesignation.Value);
         }
     }
 }
--- EletricGo/Domain/Warehouse/WarehouseDTO.cs
+++ EletricGo/Domain/Warehouse/WarehouseDTO.cs
@@ -3,14 +3,14 @@ namespace EletricGo.Domain.Warehouse
     public class WarehouseDTO
     {
         public Guid warehouseID { get; set; }
-        public string adress { get; set; }
+        public string address { get; set; }
         public string designation { get; set; }
 
 
         public WarehouseDTO(Guid warehouseID, string adress, string designation)
         {
             this.warehouseID = warehouseID;
-            this.adress = adress;
+            this.address = address;
             this.designation = designation;
             
         }
