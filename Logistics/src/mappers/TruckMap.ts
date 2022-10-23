dd7a2efe3453e23523ce2f3990a567a5c376259c
N0R70N <1210824@isep.ipp.pt>
2022-10-23 12:26:02 +0100

    Serviço REST de dados mestres de logística - Criar, Listar e Editar Camião #6 created truck map

---
+++ Logistics/src/mappers/TruckMap.ts
@@ -0,0 +1,42 @@
+import { Mapper } from "../core/infra/Mapper";
+
+import { Document, Model } from 'mongoose';
+
+import { ITruckDTO } from "../dto/ITruckDTO";
+import { Truck } from "../domain/truck/Truck";
+
+import { UniqueEntityID } from "../core/domain/UniqueEntityID";
+
+export class TruckMap extends Mapper<Truck> {
+    
+    public static toDTO(truck: Truck): ITruckDTO {
+        return {
+            id: truck.id.toString(),
+            tare: truck.tare.tare,
+            capacity: truck.capacity.capacity,
+            maxBatteryCapacity: truck.maxBatteryCapacity.capacity,
+            autonomy: truck.autonomy.autonomy,
+            fastChargeTime: truck.fastChargeTime.time,
+        } as ITruckDTO;
+    }
+
+    public static toDomain(truck: any): Truck {
+        const truckOrError = Truck.create(
+                truck,
+                new UniqueEntityID(truck._id),
+        );
+        truckOrError.isFailure ? console.log(truckOrError.error) : '';
+        return truckOrError.isSuccess ? truckOrError.getValue() : null;
+    }
+
+    public static toPersistence(truck: Truck): any {
+        return {
+            _id: truck.id.toString(),
+            tare: truck.tare.tare,
+            capacity: truck.capacity.capacity,
+            maxBatteryCapacity: truck.maxBatteryCapacity.capacity,
+            autonomy: truck.autonomy.autonomy,
+            fastChargeTime: truck.fastChargeTime.time,
+        };
+    }
+}
