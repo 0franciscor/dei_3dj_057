9029eb3c8836f9c37946ac16cba6ab77fe203207
N0R70N <1210824@isep.ipp.pt>
2022-10-23 12:25:02 +0100

    Serviço REST de dados mestres de logística - Criar, Listar e Editar Camião #6 created truck route

---
+++ Logistics/src/api/routes/truckRoute.ts
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+import { Container } from 'typedi';
+
+
+import config from "../../../config";
+import ITruckController from '../../controllers/IControllers/ITruckController';
+
+const route = Router();
+
+export default (app: Router) => {
+   
+    app.use('/truck', route);
+
+    const ctrl = Container.get(config.controllers.truck.name) as ITruckController;
+
+    route.get('/', (req, res, next) =>  ctrl.getTruck(req, res, next));
+
+
+    route.post('/', (req, res, next) => ctrl.createTruck(req, res, next));
+
+
+    route.put('/', (req, res, next) => {     
+        ctrl.updateTruck(req, res, next);
+    });
+
+    route.delete('/', (req, res, next) => {
+        ctrl.deleteTruck(req, res, next);
+    });
+
+
+}
