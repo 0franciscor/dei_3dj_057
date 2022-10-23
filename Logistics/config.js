fbd98b85c4b78fee858883d8882f7925d76f3a69
N0R70N <1210824@isep.ipp.pt>
2022-10-23 12:22:06 +0100

    Serviço REST de dados mestres de logística - Criar, Listar e Editar Camião #6 created config.js

--- Logistics/config.js
+++ Logistics/config.js
@@ -1,6 +1,24 @@
 import dotenv from 'dotenv';
 
+// Set the NODE_ENV to 'development' by default
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const envFound = dotenv.config();
+if (!envFound) {
+  // This error should crash whole process
+
+  throw new Error("⚠️  Couldn't find .env file  ⚠️");
+}
 export default {
+
+  port: parseInt(process.env.PORT, 10) || 3000,
+
+  logs: {
+    level: process.env.LOG_LEVEL || 'info',
+  },
+  
+  databaseURL: process.env.MONGODB_URI || "mongodb://localhost:27017/test",
+
   /**
    * API configs
    */
