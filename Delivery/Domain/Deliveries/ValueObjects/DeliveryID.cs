0ce97d2bca6fb58b57a9db81bbaaec97ad6924ae
Francisco Redol <1201239@isep.ipp.pt>
2022-10-26 10:27:16 +0100

    #5 ValueObject DeliveryID

---
+++ Delivery/Domain/Deliveries/ValueObjects/DeliveryID.cs
@@ -0,0 +1,37 @@
+using EletricGo.Domain.Shared;
+using System;
+
+namespace EletricGo.Domain.Deliveries
+{
+    public class DeliveryID : EntityID
+    {
+
+        private Guid deliveryID;
+
+         public DeliveryID(Guid value) : base(value)
+        {
+            this.deliveryID = value;
+        }
+
+        public DeliveryID(String value) : base(value)
+        {
+            this.deliveryID = (Guid)createFromString(value);
+        }
+
+        override
+        protected Object createFromString(String text){
+            return new Guid(text);
+        }
+        
+        override
+        public String AsString(){
+            Guid obj = (Guid) base.ObjValue;
+            return obj.ToString();
+        }
+        
+        public Guid AsGuid(){
+            return (Guid) base.ObjValue;
+        }
+
+    }    
+}
