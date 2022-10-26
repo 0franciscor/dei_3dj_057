94b3c70586705ad3025e1f08a9644830320d891d
Francisco Redol <1201239@isep.ipp.pt>
2022-10-26 10:28:04 +0100

    #5 Value Object Destination

---
+++ Delivery/Domain/Deliveries/ValueObjects/Destination.cs
@@ -0,0 +1,29 @@
+using EletricGo.Domain.Shared;
+using System.Collections.Generic;
+
+namespace EletricGo.Domain.Deliveries
+{
+    public class Destination : IValueObject
+    {
+        public string destination { get; }
+        
+        public Destination() { }
+
+        public Destination(string destination)
+        {
+            this.destination = destination;
+        }
+
+        public string AsString()
+        {
+            return destination;
+        }
+
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return destination;
+        }
+        
+    }    
+}
