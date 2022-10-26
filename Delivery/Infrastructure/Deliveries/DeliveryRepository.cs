a24d43138b6bb43c7472a42d6bb0b9edd4dbc293
Francisco Redol <1201239@isep.ipp.pt>
2022-10-26 10:35:36 +0100

    #5 Delivery repository Added

---
+++ Delivery/Infrastructure/Deliveries/DeliveryRepository.cs
@@ -0,0 +1,14 @@
+﻿using EletricGo.Domain.Deliveries;
+using EletricGo.Infrastructure.Shared;
+
+namespace EletricGo.Infrastructure.Deliveries
+{
+    public class DeliveryRepository : BaseRepository<Delivery, DeliveryID>, IDeliveryRepository
+    {
+        public DeliveryRepository(EletricGoDBContext context) : base(context.Delivery)
+        {
+
+        }
+    }
+
+}
