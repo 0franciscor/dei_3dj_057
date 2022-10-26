4f675a914dfe5a04c83f12c2a1434acc64ce11d7
Francisco Redol <1201239@isep.ipp.pt>
2022-10-26 10:36:11 +0100

    #Server BaseRepository Added

---
+++ Delivery/Infrastructure/Shared/BaseRepository.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EletricGo.Domain.Shared;
+
+namespace EletricGo.Infrastructure.Shared
+{
+    public class BaseRepository<TEntity,TEntityId> : IRepository<TEntity,TEntityId>
+    where TEntity : Entity<TEntityId>
+    where TEntityId : EntityID
+    {
+        private readonly DbSet<TEntity> _objs;
+
+        public BaseRepository(DbSet<TEntity> objs)
+        {
+            this._objs = objs ?? throw new ArgumentNullException(nameof(objs));
+
+        }
+        public async Task<TEntity> GetByID(TEntityId id)
+        {
+            //return await this._context.Categories.FindAsync(id);
+            return await this._objs
+                .Where(x => id.Equals(x.Id)).FirstOrDefaultAsync();
+        }
+        
+        public async Task<List<TEntity>> GetAll()
+        {
+            return await this._objs.ToListAsync();
+        }
+
+        public async Task<TEntity> Add(TEntity obj)
+        {
+            var ret = await this._objs.AddAsync(obj);
+            return ret.Entity;
+        }
+
+        public async Task<TEntity> Update(TEntity obj)
+        {
+            //var ret = await _objs.Update(obj);
+            //return ret.Entity;
+            return null;
+        }
+
+        public void Delete(TEntity obj)
+        {
+            this._objs.Remove(obj);
+        }
+    }
+}
