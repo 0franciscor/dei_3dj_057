1bf6d3a9cac18393038c2ad880df05d4c3d9367b
Francisco Redol <1201239@isep.ipp.pt>
2022-10-26 10:37:24 +0100

    #5 EntityTypeConfiguration Class added. Yet unstable, but changes are pendent

---
+++ Delivery/Infrastructure/Deliveries/DeliveryEntityTypeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using EletricGo.Domain.Deliveries;
+    
+
+namespace EletricGo.Infrastructure.Deliveries
+{
+    internal class DeliveryEntityTypeConfiguration : IEntityTypeConfiguration<Delivery>
+    {
+        public void Configure(EntityTypeBuilder<Delivery> builder)  
+        {
+            // cf. https://www.entityframeworktutorial.net/efcore/fluent-api-in-entity-framework-core.aspx
+
+            builder.ToTable("Delivery", SchemaNames.EletricGo);
+            builder.HasKey(b => b.Id);
+            builder.OwnsOne(b => b.deliveryDate).Property(b => b.date).HasColumnName("deliveryDate");
+            builder.OwnsOne(b => b.loadTime).Property(b => b.time).HasColumnName("loadTime");
+            builder.OwnsOne(b => b.unloadTime).Property(b => b.time).HasColumnName("unloadTime");
+            builder.OwnsOne(b => b.destination).Property(b => b.destination).HasColumnName("destination");
+            builder.OwnsOne(b => b.deliveryMass).Property(b => b.mass).HasColumnName("deliveryMass");
+            //builder.Property<bool>("_active").HasColumnName("Active");
+        }
+    }
+}
