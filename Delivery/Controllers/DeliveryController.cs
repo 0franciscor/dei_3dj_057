ea132cd55614c4080f51a7dcfb118afcfd8740a2
Francisco Redol <1201239@isep.ipp.pt>
2022-10-26 10:30:46 +0100

    #5 DeliveryController adicionado. Restam modificações e condições para tornar a sua execução "bulletproof"

---
+++ Delivery/Controllers/DeliveryController.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+using EletricGo.Domain.Deliveries;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
+
+namespace EletricGo.Controllers
+{
+
+    [Route("api/deliveries")]
+    [ApiController]
+
+    public class DeliveryController : ControllerBase
+    {
+        private readonly DeliveryService _deliveryService;
+
+        public DeliveryController(DeliveryService service)
+        {
+            _deliveryService = service;
+        }
+
+        [HttpGet("list")]
+        public async Task<ActionResult<List<DeliveryDTO>>> Get()
+        {
+            return await _deliveryService.getDeliveries();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DeliveryDTO>> GetByID(DeliveryID id)
+        {
+            return await _deliveryService.getDelivery(id);
+        }
+
+        [HttpPost("createDelivery")]
+        public async Task<ActionResult<DeliveryDTO>> Post([FromBody] DeliveryDTO dto)
+        {   
+            var delivery = await _deliveryService.createDelivery(dto);
+            return CreatedAtAction(nameof(GetByID), new { id = delivery.deliveryID}, delivery);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<DeliveryDTO>> Put(string id, [FromBody] DeliveryDTO dto)
+        {
+            return await _deliveryService.updateDelivery(id, dto);
+        }
+        
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<DeliveryDTO>> Delete(string id)
+        {
+            return await _deliveryService.deleteDelivery(id);
+        }
+    }
+    
+}
