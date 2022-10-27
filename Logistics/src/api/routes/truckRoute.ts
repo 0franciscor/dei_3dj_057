5b32502bed07f3c9a587767648e11a6261d7bbb9
N0R70N <1210824@isep.ipp.pt>
2022-10-27 18:28:12 +0100

    Serviço REST de dados mestres de logística - Criar, Listar e Editar Camião #6 Implemented 'celebrate' to truck

--- Logistics/src/api/routes/truckRoute.ts
+++ Logistics/src/api/routes/truckRoute.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { Container } from 'typedi';
-
+import { celebrate, Joi } from 'celebrate';
 
 import config from "../../../config";
 import ITruckController from '../../controllers/IControllers/ITruckController';
@@ -17,17 +17,38 @@ export default (app: Router) => {
 
     route.get('/all', (req, res, next) =>  ctrl.getAllTrucks(req, res, next));
 
+    route.post('/',
+        celebrate({
+            body: Joi.object({
+                truckID: Joi.string().required(),
+                tare: Joi.number().required(),
+                capacity: Joi.number().required(),
+                maxBatteryCapacity: Joi.number().required(),
+                autonomy: Joi.number().required(),
+                fastChargeTime: Joi.number().required()
+            })
+        }),
+        
+        (req, res, next) => ctrl.createTruck(req, res, next)
+    );
 
-    route.post('/', (req, res, next) => ctrl.createTruck(req, res, next));
 
+    route.put('/',
+        celebrate({
+            body: Joi.object({
+                truckID: Joi.string().required(),
+                tare: Joi.number(),
+                capacity: Joi.number(),
+                maxBatteryCapacity: Joi.number(),
+                autonomy: Joi.number(),
+                fastChargeTime: Joi.number()
+            })
+        }),
+        
+        (req, res, next) => ctrl.updateTruck(req, res, next)
+    );
 
-    route.put('/', (req, res, next) => {     
-        ctrl.updateTruck(req, res, next);
-    });
-
-    route.delete('/', (req, res, next) => {
-        ctrl.deleteTruck(req, res, next);
-    });
+    route.delete('/', (req, res, next) => {ctrl.deleteTruck(req, res, next)});
 
 
 }
