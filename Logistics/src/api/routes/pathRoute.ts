613501623859d6b12c0a4912b5824b98d8f4acc3
NunoBarbosa20 <1210822@isep.ipp.pt>
2022-11-02 12:34:34 +0000

    #7 modified path and n3 puml

--- Logistics/src/api/routes/pathRoute.ts
+++ Logistics/src/api/routes/pathRoute.ts
@@ -2,18 +2,52 @@ import  config  from "../../../config";
 import IPathController from "../../controllers/IControllers/IPathController";
 import { Router } from 'express';
 import { Container } from 'typedi';
+import { celebrate, Joi } from "celebrate";
 
 const route = Router();
+const cors = require('cors');
 
 export default(app: Router)=>{
-    app.use('/path',route);
+  app.use('/path',route);
+  app.use(cors({
+      methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+    }));
 
-    const ctrl= Container.get(config.controllers.path.name) as IPathController;
+  const ctrl= Container.get(config.controllers.path.name) as IPathController;
 
-    route.get('/',(req,res,next)=> ctrl.getPath(req,res,next));
-    route.get('/',(req,res,next)=> ctrl.getAllPaths(req,res,next));
-    route.get('/',(req,res,next)=> ctrl.createPath(req,res,next));
-    route.get('/',(req,res,next)=> ctrl.updatePath(req,res,next));
-    route.get('/',(req,res,next)=> ctrl.deletePath(req,res,next));
+  route.get('/',(req,res,next)=> ctrl.getPath(req,res,next));
+  route.get('/all',(req,res,next)=> ctrl.getAllPaths(req,res,next));
+    
+  route.post('/',
+    celebrate({
+      body: Joi.object({
+          pathID:Joi.string().required(),
+          startWHId: Joi.string().required(),
+          destinatioWHId: Joi.string().required(),
+          pathDistance: Joi.number().required(),
+          pathTravelTime: Joi.number().required(),
+          wastedEnergy: Joi.number().required(),
+          extraTravelTime: Joi.number().required()
+        })
+    }),
+      (req,res,next)=> ctrl.createPath(req,res,next)
+    );
+    
+  route.put('/',
+    celebrate({
+      body: Joi.object({
+        pathID:Joi.string().required(),
+        startWHId: Joi.string().required(),
+        destinatioWHId: Joi.string().required(),
+        pathDistance: Joi.number().required(),
+        pathTravelTime: Joi.number().required(),
+        wastedEnergy: Joi.number().required(),
+        extraTravelTime: Joi.number().required()
+      })
+    }),(req,res,next)=> ctrl.updatePath(req,res,next));
+  
+  
+    route.delete('/',(req,res,next)=> ctrl.deletePath(req,res,next));
+    
 
 }
