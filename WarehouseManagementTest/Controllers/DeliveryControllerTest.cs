1e2c516f1911554836e0b45935c69f85ee8b7c0d
Francisco Redol <1201239@isep.ipp.pt>
2022-11-02 23:06:07 +0000

    #5 Class de Teste de Integração Adicionada

---
+++ WarehouseManagementTest/Controllers/DeliveryControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using EletricGo.Controllers;
+using EletricGo.Domain.Deliveries;
+using Moq;
+using EletricGo.Domain.Shared;
+
+namespace DDDNetCoreTests.Controllers.Deliveries
+{
+    internal class DeliveryControllerTest
+    {
+        private string? id;
+
+        private DateTime deliveryDate;
+
+        private float loadTime;
+
+        private float unloadTime;
+
+        private string? destination;
+
+        private float deliveryMass;
+
+        [SetUp]
+        public void Setup()
+        {
+            id = "testID";
+            deliveryDate = new DateTime(2020, 12, 12);
+            loadTime = 10;
+            unloadTime = 20;
+            destination = "testDestination";
+            deliveryMass = 30;
+
+        }
+
+        
+
+    }
+}
