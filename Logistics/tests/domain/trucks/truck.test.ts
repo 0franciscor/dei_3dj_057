dfffd65c4502788b1b25a294ff47fbc460b1c6c0
N0R70N <1210824@isep.ipp.pt>
2022-11-02 17:47:43 +0000

    Unit Testing - Truck #50 added more tests to truck

--- Logistics/tests/domain/trucks/truck.test.ts
+++ Logistics/tests/domain/trucks/truck.test.ts
@@ -12,14 +12,14 @@ describe("Create Truck", () => {
                 maxBatteryCapacity: 1,
                 tare: 1,
                 id: '',
-                truckID: 'gandaTruck',
+                truckID: 'truckID',
                 fastChargeTime: 1
             });
             expect(truck.getValue().autonomy.autonomy).to.equal(1);
             expect(truck.getValue().capacity.capacity).to.equal(1);
             expect(truck.getValue().maxBatteryCapacity.capacity).to.equal(1);
             expect(truck.getValue().tare.tare).to.equal(1);
-            expect(truck.getValue().truckID.id).to.equal('gandaTruck');
+            expect(truck.getValue().truckID.id).to.equal('truckID');
             expect(truck.getValue().fastChargeTime.time).to.equal(1);
         });
     
@@ -27,17 +27,86 @@ describe("Create Truck", () => {
 
 describe ("Create an invalid truck", () => {
             
-        it("truck should be null", async () => {
+        it("truck should return error when autonomy <=0", async () => {
             const truck = Truck.create({
-                autonomy: NaN,
-                capacity: NaN,
-                maxBatteryCapacity: NaN,
-                tare: NaN,
+                autonomy: 0,
+                capacity: 1,
+                maxBatteryCapacity: 1,
+                tare: 1,
+                id: '',
+                truckID: 'truckID',
+                fastChargeTime: 1
+            });
+            expect(truck.isFailure).to.equal(true);
+
+        });    
+
+        it("truck should return error when capacity <=0", async () => {
+            const truck = Truck.create({
+                autonomy: 1,
+                capacity: 0,
+                maxBatteryCapacity: 1,
+                tare: 1,
+                id: '',
+                truckID: 'truckID',
+                fastChargeTime: 1
+            });
+            expect(truck.isFailure).to.equal(true);
+
+        });
+
+        it("truck should return error when maxBatteryCapacity <=0", async () => {
+            const truck = Truck.create({
+                autonomy: 1,
+                capacity: 1,
+                maxBatteryCapacity: 0,
+                tare: 1,
+                id: '',
+                truckID: 'truckID',
+                fastChargeTime: 1
+            });
+            expect(truck.isFailure).to.equal(true);
+
+        });
+
+        it("truck should return error when tare <=0", async () => {
+            const truck = Truck.create({
+                autonomy: 1,
+                capacity: 1,
+                maxBatteryCapacity: 1,
+                tare: 0,
+                id: '',
+                truckID: 'truckID',
+                fastChargeTime: 1
+            });
+            expect(truck.isFailure).to.equal(true);
+
+        });
+
+        it("truck should return error when truckID is invalid", async () => {
+            const truck = Truck.create({
+                autonomy: 1,
+                capacity: 1,
+                maxBatteryCapacity: 1,
+                tare: 1,
                 id: '',
                 truckID: '',
-                fastChargeTime: NaN
+                fastChargeTime: 1
             });
-            
+            expect(truck.isFailure).to.equal(true);
+
+        });
+
+        it("truck should return error when fastChargeTime is <=0", async () => {
+            const truck = Truck.create({
+                autonomy: 1,
+                capacity: 1,
+                maxBatteryCapacity: 1,
+                tare: 1,
+                id: '',
+                truckID: 'truckID',
+                fastChargeTime: 0
+            });
             expect(truck.isFailure).to.equal(true);
 
         });
