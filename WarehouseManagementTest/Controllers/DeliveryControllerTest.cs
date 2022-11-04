f14db3549cf3c8d375d1ff09cd77237de7dc257f
Francisco Redol <1201239@isep.ipp.pt>
2022-11-04 16:18:41 +0000

    #5 Teste Para GetByID adicionado

--- WarehouseManagementTest/Controllers/DeliveryControllerTest.cs
+++ WarehouseManagementTest/Controllers/DeliveryControllerTest.cs
@@ -3,8 +3,9 @@ using EletricGo.Domain.Deliveries;
 using Moq;
 using EletricGo.Domain.Shared;
 
-namespace DDDNetCoreTests.Controllers.Deliveries
+namespace WarehouseManagementTest.Controllers.Deliveries
 {
+    [TestFixture]
     internal class DeliveryControllerTest
     {
         private string? id;
@@ -62,12 +63,30 @@ namespace DDDNetCoreTests.Controllers.Deliveries
 
             for(int i = 0; i < expectedList.Count; i++)
             {
-                Assert.That(resultList.Value[i].deliveryID, Is.EqualTo(expectedList[i].deliveryID));
+                Assert.That(resultList.Value[i].deliveryID, Is.EqualTo(expectedList[i].deliveryID)); 
             }
-
         }
-                
-                
+
+        [Test]
+        public async Task GetByIDTest()
+        {
+            var delivery = new Delivery(id, new DeliveryDate(deliveryDate), new LoadTime(loadTime), new UnloadTime(unloadTime), new Destination(destination), new DeliveryMass(deliveryMass));
+            var deliveryExpected = delivery.toDeliveryDTO();
+            var deliveryID = new DeliveryID(id);
+
+            var mockUnit = new Mock<IUnitOfWork>();
+            var mockRepository = new Mock<IDeliveryRepository>();
+            var deliveryServiceMock = new Mock<DeliveryService>(mockUnit.Object, mockRepository.Object);
+
+            deliveryServiceMock.Setup(repo => repo.GetDelivery(deliveryID)).ReturnsAsync(deliveryExpected);
+
+            var deliveryController = new DeliveryController(deliveryServiceMock.Object);
+
+            var deliveryResult = await deliveryController.GetByID(id);
+
+            Assert.AreEqual(deliveryResult.Value.deliveryID, deliveryExpected.deliveryID);
+        }
+
         
 
     }
