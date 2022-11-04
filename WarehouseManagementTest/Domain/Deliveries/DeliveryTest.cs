ce08cee5a369031dca68a4441c2a69a0ba0cdf5f
Francisco Redol <1201239@isep.ipp.pt>
2022-11-04 23:48:17 +0000

    #5 Massa da Entrega Testada

--- WarehouseManagementTest/Domain/Deliveries/DeliveryTest.cs
+++ WarehouseManagementTest/Domain/Deliveries/DeliveryTest.cs
@@ -7,7 +7,7 @@ namespace WarehouseManagementTest.Domain.Deliveries
     internal class DeliveryTest
     {
         [Test]
-        public void CreateDeliveryDateInSuccess()
+        public void CreateDeliveryDateInsuccess()
         {
             var ex = Assert.Throws<BusinessRuleValidationException>(() => new DeliveryDate(new DateTime(2020, 12, 12)));
             Assert.That(ex.Message, Is.EqualTo("Date cannot be in the past."));
@@ -15,12 +15,28 @@ namespace WarehouseManagementTest.Domain.Deliveries
 
 
         [Test]
-        public void CreateDeliveryDateInuccess()
+        public void CreateDeliveryDateSuccess()
         {
             var datetime = new DateTime(2023, 12, 12);
             var deliveryDate = new DeliveryDate(datetime);
 
             Assert.That(datetime, Is.EqualTo(deliveryDate.AsDateTime()));
+        }
+
+        [Test]
+        public void CreateDeliveryMassInsuccess()
+        {
+            var ex = Assert.Throws<BusinessRuleValidationException>(() => new DeliveryMass(-3));
+            Assert.That(ex.Message, Is.EqualTo("Mass must be greater than 0"));
+        }
+
+        [Test]
+        public void CreateDeliveryMassSuccess()
+        {
+            float mass = 15f;
+            var deliveryMass = new DeliveryMass(mass);
+
+            Assert.That(mass, Is.EqualTo(deliveryMass.AsFloat()));
         }
     }
 }
