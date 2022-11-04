c037c71382f380f206e0abf652aa24f4c570af26
Francisco Redol <1201239@isep.ipp.pt>
2022-11-04 16:32:46 +0000

    #5 - Adicionado Métodos que faltavam dar override, (GetHashCode e Equals)

--- WarehouseManagement/Domain/Deliveries/ValueObjects/DeliveryMass.cs
+++ WarehouseManagement/Domain/Deliveries/ValueObjects/DeliveryMass.cs
@@ -19,6 +19,22 @@ namespace EletricGo.Domain.Deliveries
             return mass;
         }
 
+        override
+        public bool Equals(object obj)
+        {
+            if (obj == null || !(obj is DeliveryMass))
+                return false;
+
+            var deliveryMass = (DeliveryMass)obj;
+            return this.mass == deliveryMass.mass;
+        }
+
+        override
+        public int GetHashCode()
+        {
+            return mass.GetHashCode();
+        }
+
         protected override IEnumerable<object> GetEqualityComponents()
         {
             yield return mass;
