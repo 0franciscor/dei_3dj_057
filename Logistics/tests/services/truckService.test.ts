b3673c2827560e4a84a4e5b6d66de41506666dc4
N0R70N <1210824@isep.ipp.pt>
2022-11-05 21:58:11 +0000

    Integration Testing - Truck #49 Finished truck service

--- Logistics/tests/services/truckService.test.ts
+++ Logistics/tests/services/truckService.test.ts
@@ -8,6 +8,8 @@ import 'mocha';
 import {expect} from "chai";
 import { TruckMap } from "../../src/mappers/TruckMap";
 import { Truck } from "../../src/domain/truck/Truck";
+import { ITruckPersistence } from "../../src/dataschema/ITruckPersistence";
+import { Document, FilterQuery, Model } from 'mongoose';
 
 describe('TruckService Unit Tests', () => {
 
@@ -298,6 +300,249 @@ describe('TruckService Unit Tests', () => {
         const answer = await truckService.deleteTruck(body.truckID);
 
         expect(answer.errorValue()).to.equal("Truck not found");
+
+    });
+
+});
+
+describe("TruckService + TruckRepo Integration Test", () => {
+    const sandbox = sinon.createSandbox();
+    beforeEach(() => {
+        Container.reset();
+
+        
+        let truckSchemaInstance = require('../../src/persistence/schemas/truckSchema').default;
+        Container.set("truckSchema", truckSchemaInstance);
+
+        let truckRepoClass = require('../../src/repos/truckRepo').default;
+        let truckRepoInstance = Container.get(truckRepoClass);
+        Container.set("TruckRepo", truckRepoInstance);
+
+    });
+    
+    afterEach(() => {
+        sinon.restore();
+        sandbox.restore();
+    });
+
+    it('exists return true', async () => {
+        
+        let body = {
+            truckID: "truckID",
+            tare: 1,
+            capacity: 1,
+            maxBatteryCapacity: 1,
+            autonomy: 1,
+            fastChargeTime: 1
+        };
+
+        let truckSchemaInstance = Container.get("truckSchema");
+        sinon.stub(truckSchemaInstance, 'findOne').returns(Promise.resolve(body));
+
+
+        let truckRepoInstance = Container.get("TruckRepo");
+        const truckRepoSpy = sinon.spy(truckRepoInstance, 'getTruckById');
+        
+        const truckService = new TruckService(truckRepoInstance as ITruckRepo);
+        const answer = await truckService.exist(body.truckID);
+        expect(answer.getValue()).to.equal(true);
+        sinon.assert.calledOnce(truckRepoSpy);
+
+    });
+
+    it('createTruck returns truck', async () => {
+        
+        let body = {
+            truckID: "truckID",
+            tare: 1,
+            capacity: 1,
+            maxBatteryCapacity: 1,
+            autonomy: 1,
+            fastChargeTime: 1
+        };
+
+        let body2 = {
+            domainId: "id",
+            truckID: "truckID",
+            tare: 1,
+            capacity: 1,
+            maxBatteryCapacity: 1,
+            autonomy: 1,
+            fastChargeTime: 1
+        }as ITruckPersistence;
+
+        let truckSchemaInstance = Container.get("truckSchema");
+        sinon.stub(truckSchemaInstance, 'findOne').returns(Promise.resolve(null));
+        sinon.stub(truckSchemaInstance, 'create').returns(Promise.resolve(body2 as ITruckPersistence));
+
+
+        let truckRepoInstance = Container.get("TruckRepo");
+        const truckRepoSpy = sinon.spy(truckRepoInstance, 'getTruckById');
+        const truckRepoSpy2 = sinon.spy(truckRepoInstance, 'save');
+        
+        const truckService = new TruckService(truckRepoInstance as ITruckRepo);
+        const answer = await truckService.createTruck(body as ITruckDTO);
+        expect(answer.getValue().truckID).to.equal(body.truckID);
+        expect(answer.getValue().tare).to.equal(body.tare);
+        expect(answer.getValue().capacity).to.equal(body.capacity);
+        expect(answer.getValue().maxBatteryCapacity).to.equal(body.maxBatteryCapacity);
+        expect(answer.getValue().autonomy).to.equal(body.autonomy);
+        expect(answer.getValue().fastChargeTime).to.equal(body.fastChargeTime);
+        sinon.assert.calledOnce(truckRepoSpy)
+        sinon.assert.calledOnce(truckRepoSpy2)
+
+    });
+
+
+    
+    it('getTruck returns truck', async () => {
+        
+        let body = {
+            id:"id",
+            truckID: "truckID",
+            tare: 1,
+            capacity: 1,
+            maxBatteryCapacity: 1,
+            autonomy: 1,
+            fastChargeTime: 1
+        };
+
+        let truckSchemaInstance = Container.get("truckSchema");
+        sinon.stub(truckSchemaInstance, 'findOne').returns(Promise.resolve(body));
+
+
+        let truckRepoInstance = Container.get("TruckRepo");
+        const truckRepoSpy = sinon.spy(truckRepoInstance, 'getTruckById');
+        
+        const truckService = new TruckService(truckRepoInstance as ITruckRepo);
+        const answer = await truckService.getTruck(body.truckID);
+        expect(answer.getValue().truckID).to.equal(body.truckID);
+        expect(answer.getValue().tare).to.equal(body.tare);
+        expect(answer.getValue().capacity).to.equal(body.capacity);
+        expect(answer.getValue().maxBatteryCapacity).to.equal(body.maxBatteryCapacity);
+        expect(answer.getValue().autonomy).to.equal(body.autonomy);
+        expect(answer.getValue().fastChargeTime).to.equal(body.fastChargeTime);
+        sinon.assert.calledOnce(truckRepoSpy);
+    });
+    
+    it('getAllTrucks returns list', async () => {
+        
+        
+            let body = [{
+                truckID: "truckID1",
+                tare: 1,
+                capacity: 1,
+                maxBatteryCapacity: 1,
+                autonomy: 1,
+                fastChargeTime: 1,
+                id: "id"
+            },{
+                truckID: "truckID2",
+                tare: 1,
+                capacity: 1,
+                maxBatteryCapacity: 1,
+                autonomy: 1,
+                fastChargeTime: 1,
+                id: "id"
+            }];
+
+        let truckSchemaInstance = Container.get("truckSchema");
+        sinon.stub(truckSchemaInstance, 'find').returns(Promise.resolve(body));
+
+
+        let truckRepoInstance = Container.get("TruckRepo");
+        const truckRepoSpy = sinon.spy(truckRepoInstance, 'getAllTrucks');
+        
+        const truckService = new TruckService(truckRepoInstance as ITruckRepo);
+        const answer = await truckService.getAllTrucks();
+        expect(answer.getValue().length).to.equal(2);
+        sinon.assert.calledOnce(truckRepoSpy);
+    });
+
+
+    
+    it('update returns truck', async () => {
+        
+        let body = {
+            id:"id",
+            truckID: "truckID",
+            tare: 1,
+            capacity: 1,
+            maxBatteryCapacity: 1,
+            autonomy: 1,
+            fastChargeTime: 1
+        };
+
+        const body2 = {
+            domainId: "id",
+            truckID: "truckID",
+            tare: 1,
+            capacity: 1,
+            maxBatteryCapacity: 1,
+            autonomy: 1,
+            fastChargeTime: 1,
+            save() { return this; }
+        } as ITruckPersistence & Document<any, any, any>;
+
+        let truckSchemaInstance = Container.get("truckSchema");
+        sinon.stub(truckSchemaInstance, 'findOne').returns(Promise.resolve(body2 as ITruckPersistence & Document<any, any, any>));
+
+
+        let truckRepoInstance = Container.get("TruckRepo");
+        const truckRepoSpy = sinon.spy(truckRepoInstance, 'getTruckById');
+        const truckRepoSpy2 = sinon.spy(truckRepoInstance, 'save');
+        
+        const truckService = new TruckService(truckRepoInstance as ITruckRepo);
+        const answer = await truckService.updateTruck(body as ITruckDTO);
+        expect(answer.getValue().truckID).to.equal(body.truckID);
+        expect(answer.getValue().tare).to.equal(body.tare);
+        expect(answer.getValue().capacity).to.equal(body.capacity);
+        expect(answer.getValue().maxBatteryCapacity).to.equal(body.maxBatteryCapacity);
+        expect(answer.getValue().autonomy).to.equal(body.autonomy);
+        expect(answer.getValue().fastChargeTime).to.equal(body.fastChargeTime);
+        sinon.assert.calledOnce(truckRepoSpy);
+        sinon.assert.calledOnce(truckRepoSpy2);
+    });
+
+    it('delete returns truck', async () => {
+        
+        let body = {
+            id:"id",
+            truckID: "truckID",
+            tare: 1,
+            capacity: 1,
+            maxBatteryCapacity: 1,
+            autonomy: 1,
+            fastChargeTime: 1
+        };
+
+        let body2 = {
+            domainId: "id",
+            truckID: "truckID",
+            tare: 1,
+            capacity: 1,
+            maxBatteryCapacity: 1,
+            autonomy: 1,
+            fastChargeTime: 1
+        }as ITruckPersistence;
+
+        let truckSchemaInstance = Container.get("truckSchema");
+        sinon.stub(truckSchemaInstance, 'findOne').returns(Promise.resolve(body2 as ITruckPersistence & Document<any, any, any>));
+        sinon.stub(truckSchemaInstance, 'deleteOne').returns(Promise.resolve(body2 as ITruckPersistence & Document<any, any, any>));
+
+
+        let truckRepoInstance = Container.get("TruckRepo");
+        const truckRepoSpy = sinon.spy(truckRepoInstance, 'delete');
+        
+        const truckService = new TruckService(truckRepoInstance as ITruckRepo);
+        const answer = await truckService.deleteTruck(body.truckID);
+        expect(answer.getValue().truckID).to.equal(body.truckID);
+        expect(answer.getValue().tare).to.equal(body.tare);
+        expect(answer.getValue().capacity).to.equal(body.capacity);
+        expect(answer.getValue().maxBatteryCapacity).to.equal(body.maxBatteryCapacity);
+        expect(answer.getValue().autonomy).to.equal(body.autonomy);
+        expect(answer.getValue().fastChargeTime).to.equal(body.fastChargeTime);
+        sinon.assert.calledOnce(truckRepoSpy);
 
     });
 
