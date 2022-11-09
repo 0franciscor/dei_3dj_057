55f9208a2c573e9f2e6ef35a590d32ef0914ed3a
Paulo Norton <1210824@isep.ipp.pt>
2022-11-09 19:56:07 +0000

    Como gestor de frota pretendo Criar um Camião #73 Added create truck page to SPA

--- SPA/src/app/app-routing.module.ts
+++ SPA/src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LogInComponent } from './components/log-in/log-in.component';
 import { CubeComponent } from './cube/cube.component';
 // import { RegisterComponent } from './components/register/register.component';
+import { CreateTruckComponent } from './components/Logistics/truck/create-truck/create-truck.component';
+
 
 const routes: Routes = [
   { path:'cube', component: CubeComponent },
-  { path: 'login', component: LogInComponent }
+  { path: 'login', component: LogInComponent },
+  { path: 'Logistics/Truck/CreateTruck', component: CreateTruckComponent },
 ];
 
 @NgModule({
