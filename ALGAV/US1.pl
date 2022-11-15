f6ad9877e2cee7b1adbe36eaf696a82bbe324f4a
manu-07-ela <114668234+manu-07-ela@users.noreply.github.com>
2022-11-15 15:45:20 +0000

    #84 - Adição de um método para verificar se o Id introduzido no caminho está contido na base de conhecimento.

--- ALGAV/US1.pl
+++ ALGAV/US1.pl
@@ -1,7 +1,12 @@
+:-consult('bc_armazens.pl').
 
-allPaths(L, LF) :- deleteMatosinhos(5,L,L2),findall(LAux, permutation(L2,LAux),L3),appendMatosinhos([5],L3,LF).
+allPaths(L, LF) :-(checkIfExistCity(L),!,deleteMatosinhos(5,L,L2),findall(LAux,
+permutation(L2,LAux),L3),appendMatosinhos([5],L3,LF)); write('One of the entered coordinates does not correspond to a warehouse that is in the system.').
 
 deleteMatosinhos(A,L,L1):- ((member(A,L),!,delete(L,A,L1)); true).
+
+checkIfExistCity([]):-!.
+checkIfExistCity([H|T]):- ((idArmazem(_,H),!,checkIfExistCity(T));false).
 
 appendMatosinhos(_,[],[]):-!.
 appendMatosinhos(A,[L|LL],[L2|T]):- appendMatosinhos(A,LL,T), append(A,L,L1), append(L1,A,L2).
