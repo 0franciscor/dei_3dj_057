3f5f296da19640f41d96ec64aaf844cfde8b2ab6
Paulo Norton <1210824@isep.ipp.pt>
2022-11-16 17:52:45 +0000

    corrected road lengths

--- UI/src/app/components/Logistics/road-network/RoadNetworkJS/node-template.ts
+++ UI/src/app/components/Logistics/road-network/RoadNetworkJS/node-template.ts
@@ -1,23 +1,38 @@
 import * as THREE from "three";
+import { warehousePosition, warehouseConnections } from "./default-data";
 export default class NodeTemplate {
     object: THREE.Group;
+    
 
-    constructor(pos:number[], connections: any[][]) {
-
+    constructor(pos:number[], connections: any[][], allWidths: number[]) {
+        console.log(warehouseConnections.matrix)
         this.object = new THREE.Group();
-        let geometry = new THREE.CircleGeometry( 1, 32 );
-        let material = new THREE.MeshBasicMaterial( { color: 0x008080, side: THREE.DoubleSide } );
-        if(connections[0][0] === 1){
-            material = new THREE.MeshBasicMaterial( { color: 0xffff00, side: THREE.DoubleSide } );
-        }
-        
-        let circle: THREE.Mesh = new THREE.Mesh(geometry, material);
-        circle.position.set(pos[0], pos[1], pos[2]);
-
         let length = 2;
+        let width=0;
+        let largestWidth=0;
+        let index=0;
 
+        let circleWidth=0;
         connections.forEach(element => {
-            let rectangleGeometry = new THREE.PlaneGeometry( 0.5, length, 32 );
+            largestWidth=0;
+            
+            warehouseConnections.matrix.forEach(whElement => {
+                
+                if(whElement[0] == index+1 || whElement[1] == index+1){
+                    width = whElement[2];
+                    if(width>largestWidth){
+                        largestWidth=width
+                    }
+                    if(largestWidth>circleWidth){
+                        circleWidth=largestWidth;
+                    }
+                }
+            });
+
+            index++;
+            
+            
+            let rectangleGeometry = new THREE.PlaneGeometry( largestWidth, length, 32 );
             let rectangleMaterial = new THREE.MeshBasicMaterial( {color:  0x40e0d0, side: THREE.DoubleSide} );
             let rectangle: THREE.Mesh = new THREE.Mesh( rectangleGeometry, rectangleMaterial );
 
@@ -34,7 +49,7 @@ export default class NodeTemplate {
 
             
             let roadLength = Math.sqrt(Math.pow((element[0]-pos[0]),2)+Math.pow((element[1]-pos[1]),2))-length*2;
-            let roadGeometry = new THREE.PlaneGeometry( 0.5, roadLength, 32 );
+            let roadGeometry = new THREE.PlaneGeometry( largestWidth, roadLength, 32 );
             let roadMaterial = new THREE.MeshBasicMaterial( {color:  0xA52A2A , side: THREE.DoubleSide} );
             let road = new THREE.Mesh(roadGeometry, roadMaterial);
   
@@ -50,7 +65,19 @@ export default class NodeTemplate {
             
             
         });
+        console.log("largestWidth: " + largestWidth)
+        let geometry = new THREE.CircleGeometry(circleWidth, 32 );
+        let material = new THREE.MeshBasicMaterial( { color: 0x008080, side: THREE.DoubleSide } );
+        if(connections[0][0] === 1){
+            material = new THREE.MeshBasicMaterial( { color: 0xffff00, side: THREE.DoubleSide } );
+        }
         
+
+
+        let circle: THREE.Mesh = new THREE.Mesh(geometry, material);
+        circle.position.set(pos[0], pos[1], pos[2]);
+
+
 
         this.object.add(circle);
         
