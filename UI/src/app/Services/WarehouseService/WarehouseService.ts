00864b9179acd97595fbbd9ea14abebf5c8e928b
manu-07-ela <114668234+manu-07-ela@users.noreply.github.com>
2022-11-22 13:14:34 +0000

    #80 - Modelação dos armazéns em colaboração com o @0franciscor.

--- UI/src/app/Services/WarehouseService/WarehouseService.ts
+++ UI/src/app/Services/WarehouseService/WarehouseService.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import fetch from 'node-fetch';
+import * as http from "http";
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +10,18 @@ export class WarehouseService {
   constructor() { }
 
   async createWarehouse(warehouse: any) {
-    const url = 'http://localhost:5000/api/warehouses/';
+    const url = 'https://localhost:5001/api/warehouses';
+    //const http = require('https')
     const data = warehouse;
-    // const httpAgent = new http.Agent({ rejectUnauthorized: false });
+    //const httpAgent = new http.Agent({ rejectUnauthorized: false });
     fetch(url, {
       method: 'POST',
       body: JSON.stringify(data),
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        "rejectUnauthorized":"false"
       },
-      // agent: httpAgent
+      //agent: httpAgent
     })
   }
 
--- UI/src/app/components/Logistics/road-network/RoadNetworkJS/node-template.ts
+++ UI/src/app/components/Logistics/road-network/RoadNetworkJS/node-template.ts
@@ -1,85 +1,100 @@
-import * as THREE from "three";
+import * as THREE from 'three';
+import { ObjectLoader } from 'three';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
 import { warehousePosition, warehouseConnections } from "./default-data";
+
 export default class NodeTemplate {
     object: THREE.Group;
-    
 
-    constructor(pos:number[], connections: any[][], allWidths: number[]) {
+    constructor(pos: number[], connections: any[][], allWidths: number[]) {
+
         console.log(warehouseConnections.matrix)
         this.object = new THREE.Group();
         let length = 2;
-        let width=0;
-        let largestWidth=0;
-        let index=0;
+        let width = 0;
+        let largestWidth = 0;
+        let index = 0;
 
-        let circleWidth=0;
+        let circleWidth = 0;
         connections.forEach(element => {
-            largestWidth=0;
-            
+            largestWidth = 0;
+
             warehouseConnections.matrix.forEach(whElement => {
-                
-                if(whElement[0] == index+1 || whElement[1] == index+1){
+
+                if (whElement[0] == index + 1 || whElement[1] == index + 1) {
                     width = whElement[2];
-                    if(width>largestWidth){
-                        largestWidth=width
+                    if (width > largestWidth) {
+                        largestWidth = width
                     }
-                    if(largestWidth>circleWidth){
-                        circleWidth=largestWidth;
+                    if (largestWidth > circleWidth) {
+                        circleWidth = largestWidth;
                     }
                 }
             });
 
             index++;
-            
-            
-            let rectangleGeometry = new THREE.PlaneGeometry( largestWidth, length, 32 );
-            let rectangleMaterial = new THREE.MeshBasicMaterial( {color:  0x40e0d0, side: THREE.DoubleSide} );
-            let rectangle: THREE.Mesh = new THREE.Mesh( rectangleGeometry, rectangleMaterial );
 
-            let y2 = (Math.PI*element[1])/180;
-            let y1 = (Math.PI*pos[1])/180;
-            let x2 = (Math.PI*element[0])/180;
-            let x1 = (Math.PI*pos[0])/180;
 
-            rectangle.rotation.z= Math.atan2((y2-y1),(x2-x1))-Math.PI/2;
-            
-            rectangle.position.set(pos[0]-length/2*Math.sin(rectangle.rotation.z), pos[1]+length/2*Math.cos(rectangle.rotation.z), pos[2]);
+            let rectangleGeometry = new THREE.PlaneGeometry(largestWidth, length, 32);
+            let rectangleMaterial = new THREE.MeshBasicMaterial({ color: 0x40e0d0, side: THREE.DoubleSide });
+            let rectangle: THREE.Mesh = new THREE.Mesh(rectangleGeometry, rectangleMaterial);
+
+            let y2 = (Math.PI * element[1]) / 180;
+            let y1 = (Math.PI * pos[1]) / 180;
+            let x2 = (Math.PI * element[0]) / 180;
+            let x1 = (Math.PI * pos[0]) / 180;
+
+            rectangle.rotation.z = Math.atan2((y2 - y1), (x2 - x1)) - Math.PI / 2;
+
+            rectangle.position.set(pos[0] - length / 2 * Math.sin(rectangle.rotation.z), pos[1] + length / 2 * Math.cos(rectangle.rotation.z), pos[2]);
             this.object.add(rectangle);
 
 
-            
-            let roadLength = Math.sqrt(Math.pow((element[0]-pos[0]),2)+Math.pow((element[1]-pos[1]),2))-length*2;
-            let roadGeometry = new THREE.PlaneGeometry( largestWidth, roadLength, 32 );
-            let roadMaterial = new THREE.MeshBasicMaterial( {color:  0xA52A2A , side: THREE.DoubleSide} );
+
+            let roadLength = Math.sqrt(Math.pow((element[0] - pos[0]), 2) + Math.pow((element[1] - pos[1]), 2)) - length * 2;
+            let roadGeometry = new THREE.PlaneGeometry(largestWidth, roadLength, 32);
+            let roadMaterial = new THREE.MeshBasicMaterial({ color: 0xA52A2A, side: THREE.DoubleSide });
             let road = new THREE.Mesh(roadGeometry, roadMaterial);
-  
-            
-
-            
-            
 
 
-            road.rotation.z= Math.atan2((y2-y1),(x2-x1))-Math.PI/2;
-            road.position.set((pos[0]+element[0])/2, (pos[1]+element[1])/2, (pos[2]+element[2])/2);
+
+
+
+
+
+            road.rotation.z = Math.atan2((y2 - y1), (x2 - x1)) - Math.PI / 2;
+            road.position.set((pos[0] + element[0]) / 2, (pos[1] + element[1]) / 2, (pos[2] + element[2]) / 2);
             this.object.add(road);
-            
-            
+
+
         });
         console.log("largestWidth: " + largestWidth)
-        let geometry = new THREE.CircleGeometry(circleWidth, 32 );
-        let material = new THREE.MeshBasicMaterial( { color: 0x008080, side: THREE.DoubleSide } );
-        if(connections[0][0] === 1){
-            material = new THREE.MeshBasicMaterial( { color: 0xffff00, side: THREE.DoubleSide } );
+        let geometry = new THREE.CircleGeometry(circleWidth * 2, 32);
+        let material = new THREE.MeshBasicMaterial({ color: 0x008080, side: THREE.DoubleSide });
+        if (connections[0][0] === 1) {
+            material = new THREE.MeshBasicMaterial({ color: 0xffff00, side: THREE.DoubleSide });
         }
-        
+
 
 
         let circle: THREE.Mesh = new THREE.Mesh(geometry, material);
         circle.position.set(pos[0], pos[1], pos[2]);
 
+        let cube: THREE.Mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0xFFFFFF }));
+        cube.position.set(pos[0], pos[1], pos[2]);
+        //this.object.add(cube);
 
+        const objLoader = new FBXLoader();
+        objLoader.load('src/app/assets/warehouse/source/industrialMill.fbx',
+            (object) => {
+                this.object.add(object);
+            },
+            (error) => {
+                console.log(error)
+            });
+        //i want to use the texture i previously loaded with the objLoader
 
         this.object.add(circle);
-        
+
     }
 }
