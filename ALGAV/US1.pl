9523fa0691e51a68722c315212293490cf0b72ee
manu-07-ela <114668234+manu-07-ela@users.noreply.github.com>
2022-11-23 17:57:04 +0000

    #85 - Alteração no método 'deleteMatosinhos'.

--- ALGAV/US1.pl
+++ ALGAV/US1.pl
@@ -2,9 +2,9 @@
 :-consult('bc_factos_camiao.pl').
 :-consult('bc_entrega.pl').
 
-allPaths(L, LF) :-(checkIfCityExist(L),!,findMatosinhos(M1),deleteMatosinhos(M1,L,L2),findall(LAux,permutation(L2,LAux),L3),appendMatosinhos([M1],L3,LF)); write('One of the entered coordinates does not correspond to a warehouse that is in the system.').
+allPaths(L, LF) :-((checkIfCityExist(L),!,findMatosinhos(M1),deleteMatosinhos(M1,L,L2),findall(LAux,permutation(L2,LAux),L3),appendMatosinhos([M1],L3,LF)); write('One of the entered coordinates does not correspond to a warehouse that is in the system.')).
 
-deleteMatosinhos(A,L,L1):- ((member(A,L),!,delete(L,A,L1)); true).
+deleteMatosinhos(A,L,L1):- ((member(A,L),!,delete(L,A,L1)); L1=L).
 
 checkIfCityExist([]):-!.
 checkIfCityExist([H|T]):- ((idArmazem(_,H),!,checkIfCityExist(T));false).
