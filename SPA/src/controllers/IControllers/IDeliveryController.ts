a7900b8980c911c9c42ba25bc00b9a56642b9135
Francisco Redol <1201239@isep.ipp.pt>
2022-11-23 20:12:12 +0000

    #72 Componentes SPA

---
+++ SPA/src/controllers/IControllers/IDeliveryController.ts
@@ -0,0 +1,6 @@
+import { Request, Response, NextFunction } from 'express';
+
+export default interface IDeliveryController  {
+  createDelivery(req: Request, res: Response, next: NextFunction);
+  getAllDeliveries(req: Request, res: Response, next: NextFunction)
+}
