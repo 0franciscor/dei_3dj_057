15baba5e609359da496283d77220b68b7b16aa85
Francisco Redol <1201239@isep.ipp.pt>
2022-11-25 00:34:12 +0000

    #72 Atualização para validações

--- UI/src/app/components/WarehouseManagement/delivery/create-delivery/create-delivery.component.ts
+++ UI/src/app/components/WarehouseManagement/delivery/create-delivery/create-delivery.component.ts
@@ -1,45 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 import { DeliveryService } from "src/app/Services/DeliveryService/delivery.service";
-import { FormBuilder, FormGroup } from "@angular/forms";
-
-interface Delivery{
-  deliveryID: string;
-  deliveryDate: Date;
-  loadTime: number;
-  unloadTime: number;
-  destination: string;
-  deliveryMass: number;
-}
+import { FormBuilder, FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-create-delivery',
   templateUrl: './create-delivery.component.html',
-  styleUrls: ['./create-delivery.component.css']
+  styleUrls: ['./create-delivery.component.css'],
 })
+
 export class CreateDeliveryComponent implements OnInit {
 
   formCreateDelivery!: FormGroup;
   minDate: Date;
-  
+
   constructor(private deliveryService: DeliveryService, private fb: FormBuilder) {
-      const currentYear = new Date().getFullYear();
-      //set minimum date for today
-      this.minDate = new Date(currentYear, 0, 1);
-   }
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth();
+    const currentDay = new Date().getDate();
+    this.minDate = new Date(currentYear, currentMonth, currentDay);
+  }
 
   ngOnInit(): void {
     this.formCreateDelivery = this.fb.group({
-      deliveryID: [''],
-      deliveryDate: [''],
-      loadTime: [''],
-      unloadTime: [''],
-      destination: [''],
-      deliveryMass: [''],
+      deliveryID: new FormControl('', [Validators.required]),
+      deliveryDate: new FormControl('', [Validators.required]),
+      loadTime: new FormControl('', [Validators.required, Validators.min(0)]),
+      unloadTime: new FormControl('', [Validators.required, Validators.min(0)]),
+      destination: new FormControl('', [Validators.required]),
+      deliveryMass: new FormControl('', [Validators.required, Validators.min(0)]),
     });
   }
 
   onSubmit() {
     console.log(this.formCreateDelivery.value);
     this.deliveryService.createDelivery(this.formCreateDelivery.value);
+  }
+
+  get deliveryID() {
+    return this.formCreateDelivery.get('deliveryID');
+  }
+
+  get deliveryDate() {
+    return this.formCreateDelivery.get('deliveryDate');
+  }
+
+  get loadTime() {
+    return this.formCreateDelivery.get('loadTime');
+  }
+
+  get unloadTime() {
+    return this.formCreateDelivery.get('unloadTime');
+  }
+
+  get destination() {
+    return this.formCreateDelivery.get('destination');
+  }
+
+  get deliveryMass() {
+    return this.formCreateDelivery.get('deliveryMass');
   }
 }
