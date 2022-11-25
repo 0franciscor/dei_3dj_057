7c1ee839055a321d66f0365248636b6e13d1c7c5
manu-07-ela <114668234+manu-07-ela@users.noreply.github.com>
2022-11-25 17:08:17 +0000

    #85 - Criação do predicado dinamico infoTime.

--- ALGAV/US1.pl
+++ ALGAV/US1.pl
@@ -2,6 +2,8 @@
 :-consult('bc_factos_camiao.pl').
 :-consult('bc_entrega.pl').
 
+:-dynamic infoTime/2.
+
 allPaths(L, LF) :-((checkIfCityExist(L),!,findMatosinhos(M1),deleteMatosinhos(M1,L,L2),findall(LAux,permutation(L2,LAux),L3),appendMatosinhos([M1],L3,LF)); write('One of the entered coordinates does not correspond to a warehouse that is in the system.')).
 
 deleteMatosinhos(A,L,L1):- ((member(A,L),!,delete(L,A,L1)); L1=L).
@@ -14,9 +16,6 @@ appendMatosinhos(A,[L|LL],[L2|T]):- appendMatosinhos(A,LL,T), append(A,L,L1), ap
 
 
 findMatosinhos(M):-idArmazem('Matosinhos',M).
-
-
-
 
 weightWithDeliveries(IDTRUCK,DL,FW):- carateristicasCam(IDTRUCK,TW,CP,_,_,_),sumDeliveryWeights(DL,DW,CP),FW is TW+DW.
 
@@ -82,11 +81,14 @@ extraTimeTravel(IDTRUCK,DL,BW,[H|_],TRUCKE,FENERGY,ET):- energy(IDTRUCK,BW,H,DL,
 
 %compares best time of all paths
 
-comparePaths([],_,_,10000000.00000,_):-!.
-comparePaths([H|T],IDTRUCK,DL,BP,QL):-comparePaths(T,IDTRUCK,DL,BP1,QL),carateristicasCam(IDTRUCK,_,_,BAT,_,_), TOTALTIME is 0,analisePath(H,IDTRUCK,DL,BAT,TOTALTIME,TIME),((BP1 > TIME,!, BP is TIME,QL=H); BP is BP1).
+comparePaths([],_,_):-!.
+comparePaths([H|T],IDTRUCK,DL):-comparePaths(T,IDTRUCK,DL),carateristicasCam(IDTRUCK,_,_,BAT,_,_), TOTALTIME is 0,analisePath(H,IDTRUCK,DL,BAT,TOTALTIME,TIME),createPathsWithEnergy(TIME,H).
 
+createPathsWithEnergy(H,T):-assert(infoTime(H,T)).
+
+getAllInfoTimes(infoTime(L,T)):- setof(T,infoTime(L,T),[T|_]).
 
 appendDelivery(L,L1):- append([1], L, L2), append(L2,[1],L1).
 
 %append to list
-quickestPath(IDTRUCK,DELL,QL):-appendDelivery(DELL,DL),findAllPaths(DL,AP), comparePaths(AP,IDTRUCK,DL,_,QL).
+quickestPath(IDTRUCK,DELL,T):-appendDelivery(DELL,DL),findAllPaths(DL,AP), comparePaths(AP,IDTRUCK,DL),getAllInfoTimes(infoTime(T,_)).
