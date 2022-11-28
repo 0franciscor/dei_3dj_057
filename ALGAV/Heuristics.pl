f3d0a08f088ae54535b99c82cdb52a7efece61f0
N0R70N <1210824@isep.ipp.pt>
2022-11-28 20:57:16 +0000

    #87 Fixed Largest Mass first Heuristic to return list of warehouses with matosinhos appended at start and finish

--- ALGAV/Heuristics.pl
+++ ALGAV/Heuristics.pl
@@ -5,4 +5,8 @@
 extractMass([X],[M]):- entrega(X,_,M,_,_,_).
 extractMass([H|T],[H1|T1]):- extractMass(T,T1), entrega(H,_,H1,_,_,_).
 
-largestMassFirst([H|T],L):- extractMass([H|T],M), sort(M, M1), reverse(M1, L).
+extractWarehouse([],[Matosinhos]):- findMatosinhos(Matosinhos).
+extractWarehouse([H|T],[H1|T1]):- extractWarehouse(T,T1), entrega(_,_,H,H1,_,_).
+
+largestMassFirst(E,L):- extractMass(E,M), sort(M, M1), reverse(M1, M2), extractWarehouse(M2,L1), findMatosinhos(Matosinhos),append([Matosinhos],L1,L).
+
