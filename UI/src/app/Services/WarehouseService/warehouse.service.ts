160f79274a8cb01857ef7a8aa4251fb8eccf42eb
Paulo Norton <1210824@isep.ipp.pt>
2022-11-29 09:24:34 +0000

    temporary solution to connect project to home server

--- UI/src/app/Services/WarehouseService/warehouse.service.ts
+++ UI/src/app/Services/WarehouseService/warehouse.service.ts
@@ -13,7 +13,9 @@ export class WarehouseService {
   async getWarehouse(WarehouseId:string) {
     console.log(WarehouseId);
     const url = 'http://localhost:3001/api/warehouse/'+ WarehouseId;
-    
+    const urlBackUp = "http://5.249.66.111:3001/api/warehouse/"+WarehouseId;
+    try {
+      
     const response = await fetch(url, {
       method: 'GET',
       headers: {
@@ -24,70 +26,137 @@ export class WarehouseService {
     const data = await response.json();
 
     return data;
+    } catch {
+        
+      const response = await fetch(urlBackUp, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json'
+        }
+      });
+
+      const data = await response.json();
+
+      return data;
+    }
   }
 
   async createWarehouse(warehouse: any) {
     const url = 'http://localhost:3001/api/warehouse/create';
+    const urlBackUp = "http://5.249.66.111:3001/api/warehouse/create";
     const data = warehouse;
     
-    let answer;
+    try {
+      
+      const response = fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json'
+        },
 
-    const response = fetch(url, {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json'
-      },
-
-    })
-    console.log(response);
-    return response;
+      })
+      return response;
+      
+    } catch {
+  
+      const response = fetch(urlBackUp, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+  
+      })
+      return response;
+        
+    }
 
     
   }
 
   async getAllWarehouses() {
-    let test: any[] = [];
     const url = 'http://localhost:3001/api/warehouse/all';
-    const response = await fetch(url, {
-      method: 'GET'
-    })
-    const data = await response.json();
-    return data;
+    const urlBackUp = "http://5.249.66.111:3001/api/warehouse/all";
+    try {
+      
+      const response = await fetch(url, {
+        method: 'GET'
+      })
+      const data = await response.json();
+      return data;
+     
+    } catch {
     
+      const response = await fetch(urlBackUp, {
+        method: 'GET'
+      })
+      const data = await response.json();
+      return data;
+        
+    }
   }
 
   async updateWarehouse(warehouse: any) {
 
     const url = 'http://localhost:3001/api/warehouse/update';
+    const urlBackUp = "http://5.249.66.111:3001/api/warehouse/update";
     const data = warehouse;
 
-    console.log(data);
+    try {
 
-    const response = await fetch(url, {
-      method: 'PUT',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    })
-    
-    return response;
+      const response = await fetch(url, {
+        method: 'PUT',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      })
+      
+      return response;
+        
+    } catch {
+
+      const response = await fetch(urlBackUp, {
+        method: 'PUT',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      })
+      
+      return response;
+        
+    }
 
   }
 
   async deleteWarehouse(WarehousId: string) {
     const url = 'http://localhost:3001/api/warehouse/delete/'+WarehousId;
-    
-    const response = await fetch(url, {
-      method: 'DELETE',
-      headers: {
-        'Accept': 'application/json'
-      }
-    });
-    
-
-    return response;
+    const urlBackUp = "http://5.249.66.111:3001/api/warehouse/delete/"+WarehousId;
+    try {
+      const response = await fetch(url, {
+        method: 'DELETE',
+        headers: {
+          'Accept': 'application/json'
+        }
+      });
+      
+  
+      return response;
+        
+    } catch  {
+      const response = await fetch(urlBackUp, {
+        method: 'DELETE',
+        headers: {
+          'Accept': 'application/json'
+        }
+      });
+      
+  
+      return response;
+        
+    }
   }
 
 
