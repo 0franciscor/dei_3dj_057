2da5ed8a09925812a10c29a64f6236953dee2dfc
manu-07-ela <114668234+manu-07-ela@users.noreply.github.com>
2022-11-29 15:16:41 +0000

    #103 - Criando os métodos para passar os objetos da base de conhecimento do Prolog para Json.

--- ALGAV/inc1.pl
+++ ALGAV/inc1.pl
@@ -1,7 +1,7 @@
-:- module(inc1, [create_city/2, update_city/2, remove_city/1,
-create_truck/6, update_truck/6, remove_truck/1,
-create_delivery/6, update_delivery/6, remove_delivery/1,
-create_path/6, update_path/6, remove_path/2,
+:- module(inc1, [add_city/3, update_city/3, remove_city/1,
+create_truck/7, update_truck/7, remove_truck/1,
+create_delivery/7, update_delivery/7, remove_delivery/1,
+create_path/7, update_path/7, remove_path/2,
 count/8, resetar/0]).
                 
 :- use_module(library(http/json)).
@@ -14,52 +14,64 @@ count/8, resetar/0]).
 :- dynamic entrega/6.
 :- dynamic dadosCam_t_e_ta/6.
 
-% -------------------- City Basics ------------------------------------------
-
-create_city(Id, Name):- 
-	\+idArmazem(_, Id), assertz(idArmazem(Name, Id)).
+% ---------------------------------------------------------------------------
+add_city(Id, Name, CityJson):- \+idArmazem(_,Id), assertz(idArmazem(Name, Id)), cityprolog_tojson(Id, Name, CityJson).
     
-update_city(Id, Name):- 
-	idArmazem(_, Id), retract(idArmazem(_, Id)), assertz(idArmazem(Name, Id)).
+update_city(Id, Name, CityJson):- idArmazem(_, Id), retract(idArmazem(_, Id)), assertz(idArmazem(Name, Id)), cityprolog_tojson(Id, Name, CityJson).
     
-remove_city(Id):- 
-	idArmazem(_, Id), retract(idArmazem(_, Id)).
+remove_city(Id):- idArmazem(_, Id), retract(idArmazem(_, Id)).
 
-% -------------------- Truck Basics ------------------------------------------
+% ---------------------------------------------------------------------------
+cityprolog_tojson(Id, Name, NoJson):-
+		atom_string(Id, IdJs), atom_string(Name, NameJs),
+		NoJson=json([id = IdJs, name = NameJs]).
 
-create_truck(Id, Tare, Capacity, BateryCapacity, Autonomy,TimeToCharge):- 
-	\+caracteristicasCam(Id, _, _, _, _, _), assertz(caracteristicasCam(Id, Tare, Capacity, BateryCapacity, Autonomy, TimeToCharge)).
+% ---------------------------------------------------------------------------
+create_truck(Id, Tare, Capacity, BateryCapacity, Autonomy,TimeToCharge, TruckJson):- 
+	\+caracteristicasCam(Id, _, _, _, _, _), assertz(caracteristicasCam(Id, Tare, Capacity, BateryCapacity, Autonomy, TimeToCharge)), truckprolog_tojson(Id, Tare, Capacity, BateryCapacity, Autonomy, TimeToCharge, TruckJson).
     
-update_truck(Id, Tare, Capacity, BateryCapacity, Autonomy,TimeToCharge):- 
-	caracteristicasCam(Id, _, _, _, _, _), retract(caracteristicasCam(Id, _, _, _, _, _)), assertz(caracteristicasCam(Id, Tare, Capacity, BateryCapacity, Autonomy, TimeToCharge)).
+update_truck(Id, Tare, Capacity, BateryCapacity, Autonomy,TimeToCharge, TruckJson):- 
+	caracteristicasCam(Id, _, _, _, _, _), retract(caracteristicasCam(Id, _, _, _, _, _)), assertz(caracteristicasCam(Id, Tare, Capacity, BateryCapacity, Autonomy, TimeToCharge)), truckprolog_tojson(Id, Tare, Capacity, BateryCapacity, Autonomy, TimeToCharge, TruckJson).
     
 remove_truck(Id):- 
 	caracteristicasCam(Id, _, _, _, _, _), retract(caracteristicasCam(Id, _, _, _, _, _)).
 
-% -------------------- Delivery Basics ------------------------------------------
-
-create_delivery(Id, Date, Mass, Destination, LoadTime, UnloadTime):-
-	\+entrega(Id, _, _, _, _, _), assertz(entrega(Id, Date, Mass, Destination, LoadTime, UnloadTime)).
+% ---------------------------------------------------------------------------
+truckprolog_tojson(Id, Tare, Capacity, BateryCapacity, Autonomy,TimeToCharge, NoJson):-
+		atom_string(Id, IdJs), atom_string(Tare, TareJs), atom_string(Capacity, CapacityJs), atom_string(BateryCapacity, BateryCapacityJs), atom_string(Autonomy, AutonomyJs), atom_string(TimeToCharge, TimeToChargeJs), 
+		NoJson=json([id = IdJs, tare = TareJs, capacity = CapacityJs, bateryCapacity = BateryCapacityJs, autonomy = AutonomyJs, timeToCharge = TimeToChargeJs]).
+        
+% ---------------------------------------------------------------------------
+create_delivery(Id, Date, Mass, Destination, LoadTime, UnloadTime, DeliveryJson):-
+	\+entrega(Id, _, _, _, _, _), assertz(entrega(Id, Date, Mass, Destination, LoadTime, UnloadTime)), deliveryprolog_tojson(Id, Date, Mass, Destination, LoadTime, UnloadTime, DeliveryJson).
     
-update_delivery(Id, Date, Mass, Destination, LoadTime, UnloadTime):-
-	entrega(Id, _, _, _, _, _), retract(entrega(Id, _, _, _, _, _)), assertz(entrega(Id, Date, Mass, Destination, LoadTime, UnloadTime)).
+update_delivery(Id, Date, Mass, Destination, LoadTime, UnloadTime, DeliveryJson):-
+	entrega(Id, _, _, _, _, _), retract(entrega(Id, _, _, _, _, _)), assertz(entrega(Id, Date, Mass, Destination, LoadTime, UnloadTime)), deliveryprolog_tojson(Id, Date, Mass, Destination, LoadTime, UnloadTime, DeliveryJson).
     
 remove_delivery(Id):-
 	entrega(Id, _, _, _, _, _), retract(entrega(Id, _, _, _, _, _)).
 
-% -------------------- Path Basics ------------------------------------------
+% ---------------------------------------------------------------------------
+deliveryprolog_tojson(Id, Date, Mass, Destination, LoadTime, UnloadTime, NoJson):-
+		atom_string(Id, IdJs), atom_string(Date, DateJs), atom_string(Mass, MassJs), atom_string(Destination, DestinationJs), atom_string(LoadTime, LoadTimeJs), atom_string(UnloadTime, UnloadTimeJs), 
+		NoJson=json([id = IdJs, date = DateJs, mass = MassJs, destination = DestinationJs, loadTime = LoadTimeJs, unloadTime = UnloadTimeJs]).
 
-create_path(Truck, CityOrig, CityDest, PathTime, Energy, AditionalTime):-
+% ---------------------------------------------------------------------------
+create_path(Truck, CityOrig, CityDest, PathTime, Energy, AditionalTime, PathJson):-
 	\+dadosCam_t_e_ta(_, CityOrig, CityDest, _, _, _), assertz(dadosCam_t_e_ta(Truck, CityOrig, CityDest, PathTime, Energy, AditionalTime)).
     
-update_path(Truck, CityOrig, CityDest, PathTime, Energy, AditionalTime):-
+update_path(Truck, CityOrig, CityDest, PathTime, Energy, AditionalTime, PathJson):-
 	dadosCam_t_e_ta(_, CityOrig, CityDest, _, _, _), retract(dadosCam_t_e_ta(_, CityOrig, CityDest, _, _, _)), assertz(dadosCam_t_e_ta(Truck, CityOrig, CityDest, PathTime, Energy, AditionalTime)).
     
 remove_path(CityOrig, CityDest):-
 	dadosCam_t_e_ta(_, CityOrig, CityDest, _, _, _), retract(dadosCam_t_e_ta(_, CityOrig, CityDest, _, _, _)).
 
-% ----------------------------------------------------------------------------
+% ---------------------------------------------------------------------------
+pathprolog_tojson(Truck, CityOrig, CityDest, PathTime, Energy, AditionalTime, NoJson):-
+		atom_string(Truck, TruckJs), atom_string(CityOrig, CityOrigJs), atom_string(CityDest, CityDestJs), atom_string(PathTime, PathTimeJs), atom_string(Energy, EnergyJs), atom_string(AditionalTime, AditionalTimeJs), 
+		NoJson=json([truck = TruckJs, origin = CityOrigJs, destination = CityDestJs, totalTime = PathTimeJs, energy = EnergyJs, aditionalTime = aditionalTime]).
 
+% ---------------------------------------------------------------------------
 count(LC, LT, LD, LP, TC, TT, TD, TP):-
 		findall((_,_), idArmazem(_,_), LC),
         findall((_,_,_,_,_,_), caracteristicasCam(_,_,_,_,_,_), LT),
@@ -69,6 +81,7 @@ count(LC, LT, LD, LP, TC, TT, TD, TP):-
         length(LT, TT),
         length(LD, TD),
         length(LP, TP).
-        
+
+% ---------------------------------------------------------------------------
 resetar():-
 	retractall(idArmazem(_,_)).
--- ALGAV/server.pl
+++ ALGAV/server.pl
@@ -42,14 +42,14 @@ create_city(Request):-
 		cors_enable,
         http_read_json_dict(Request, DictIn),
         Id = DictIn.get(id), Name = DictIn.get(name),
-        (create_city(Id, Name), reply_json(_, [status(200)]), !);
+        (add_city(Id, Name, CityJson), reply_json(CityJson, [status(200)]), !);
         reply_json(_, [status(500)]).
 
 update_city(Request):-
 		cors_enable,
         http_read_json_dict(Request, DictIn),
         Id = DictIn.get(id), Name = DictIn.get(name),
-        (update_city(Id, Name), reply_json(_,  [status(200)]), !);
+        (update_city(Id, Name, CityJson), reply_json(CityJson,  [status(200)]), !);
         reply_json(_, [status(500)]).
         
 delete_city(Request):-
@@ -63,14 +63,14 @@ create_truck(Request):-
 		cors_enable,
         http_read_json_dict(Request, DictIn),
         Id = DictIn.get(id), Tare = DictIn.get(tare), Capacity = DictIn.get(capacity), BateryCapacity = DictIn.get(baterycapacity), Autonomy = DictIn.get(autonomy), TimeToCharge = DictIn.get(timeToCharge),
-        (create_truck(Id, Tare, Capacity, BateryCapacity, Autonomy,TimeToCharge), reply_json(_,  [status(200)]), !);
+        (create_truck(Id, Tare, Capacity, BateryCapacity, Autonomy,TimeToCharge, TruckJson), reply_json(TruckJson,  [status(200)]), !);
         reply_json(_, [status(500)]).
         
 update_truck(Request):-
 		cors_enable,
         http_read_json_dict(Request, DictIn),
         Id = DictIn.get(id), Tare = DictIn.get(tare), Capacity = DictIn.get(capacity), BateryCapacity = DictIn.get(baterycapacity), Autonomy = DictIn.get(autonomy), TimeToCharge = DictIn.get(timeToCharge),
-        (update_truck(Id, Tare, Capacity, BateryCapacity, Autonomy,TimeToCharge), reply_json(_,  [status(200)]), !);
+        (update_truck(Id, Tare, Capacity, BateryCapacity, Autonomy,TimeToCharge, TruckJson), reply_json(TruckJson,  [status(200)]), !);
         reply_json(_, [status(500)]).
         
 delete_truck(Request):-
@@ -84,14 +84,14 @@ create_delivery(Request):-
 		cors_enable,
         http_read_json_dict(Request, DictIn),
         Id = DictIn.get(id), Date = DictIn.get(date), Mass = DictIn.get(deliveryMass), Destination = DictIn.get(destination), LoadTime = DictIn.get(loadTime), UnloadTime = DictIn.get(unloadTime),
-        (create_delivery(Id, Date, Mass, Destination, LoadTime, UnloadTime), reply_json(_,  [status(200)]), !);
+        (create_delivery(Id, Date, Mass, Destination, LoadTime, UnloadTime, DeliveryJson), reply_json(DeliveryJson,  [status(200)]), !);
         reply_json(_, [status(500)]).
 
 update_delivery(Request):-
 		cors_enable,
         http_read_json_dict(Request, DictIn),
         Id = DictIn.get(id), Date = DictIn.get(date), Mass = DictIn.get(deliveryMass), Destination = DictIn.get(destination), LoadTime = DictIn.get(loadTime), UnloadTime = DictIn.get(unloadTime),
-        (update_delivery(Id, Date, Mass, Destination, LoadTime, UnloadTime), reply_json(_,  [status(200)]), !);
+        (update_delivery(Id, Date, Mass, Destination, LoadTime, UnloadTime, DeliveryJson), reply_json(DeliveryJson,  [status(200)]), !);
         reply_json(_, [status(500)]).
         
 delete_delivery(Request):-
@@ -105,14 +105,14 @@ create_path(Request):-
 		cors_enable,
         http_read_json_dict(Request, DictIn),
         Truck = DictIn.get(truck), CityOrig = DictIn.get(cityOrig), CityDest = DictIn.get(cityDest), PathTime = DictIn.get(pathTime), Energy = DictIn.get(energy), AditionalTime = DictIn.get(aditionalTime),
-        (create_path(Truck, CityOrig, CityDest, PathTime, Energy, AditionalTime), reply_json(_,  [status(200)]), !);
+        (create_path(Truck, CityOrig, CityDest, PathTime, Energy, AditionalTime, PathJson), reply_json(PathJson,  [status(200)]), !);
         reply_json(_, [status(500)]).
         
 update_path(Request):-
 		cors_enable,
         http_read_json_dict(Request, DictIn),
         OldCityOrig = DictIn.get(OldCityOrig), OldCityDest = DictIn.get(OldCityDest), Truck = DictIn.get(truck), PathTime = DictIn.get(pathTime), Energy = DictIn.get(energy), AditionalTime = DictIn.get(aditionalTime),
-        (update_path(OldCityOrig, OldCityDest, Truck = DictIn.get(truck), PathTime, Energy, AditionalTime), reply_json(_,  [status(200)]), !);
+        (update_path(OldCityOrig, OldCityDest, Truck, PathTime, Energy, AditionalTime), reply_json(PathJson,  [status(200)]), !);
         reply_json(_, [status(500)]).
 
 delete_path(Request):-
