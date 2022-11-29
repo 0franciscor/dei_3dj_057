cbfaa3e7d8c9d05f63a14d574656a24ce667c782
Paulo Norton <1210824@isep.ipp.pt>
2022-11-29 08:58:18 +0000

    Making UI deliveries connect to home server

--- UI/src/app/Services/DeliveryService/delivery.service.ts
+++ UI/src/app/Services/DeliveryService/delivery.service.ts
@@ -10,51 +10,109 @@ export class DeliveryService {
 
   async getDeliveries(){
     const url = "http://localhost:3001/api/delivery/all";
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json'
-      }
-    });
-    const data = await response.json();
-    return data;
+    const urlBackUp = "http://5.249.66.111:3001/api/delivery/all";
+    
+    try{
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json'
+        }
+      });
+
+      const data = await response.json();
+      return data;  
+    }
+    catch{
+      const response = await fetch(urlBackUp, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json'
+        }
+      });
+      const data = await response.json();
+      return data;
+    }
+    
+    
   }
 
   async getDelivery(deliveryID: string){
     const url = "http://localhost:3001/api/delivery/" + deliveryID;
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json'
-      }
-    });
-    const data = await response.json();
-    return data;
+    const urlBackUp = "http://5.249.66.111:3001/api/delivery/"+deliveryID;
+    try{
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json'
+        }
+      });
+      const data = await response.json();
+      return data;
+    }catch{
+      const response = await fetch(urlBackUp, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json'
+        }
+      });
+      const data = await response.json();
+      return data;
+    }
+    
   }
 
   async createDelivery(delivery: any) {
     const url = "http://localhost:3001/api/delivery/create";
+    const urlBackUp = "http://5.249.66.111:3001/api/delivery/create";
     const data = delivery;
-    const response = await fetch(url, {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    })
-    return response;
+    try{
+
+      const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      })
+      return response;
+    }catch{
+      
+      const response = await fetch(urlBackUp, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      })
+      return response;
+    }
   }
 
   async updateDelivery(delivery: any) {
     const url = "http://localhost:3001/api/delivery/update";
+    const urlBackUp = "http://5.249.66.111:3001/api/delivery/update";
+
     const data = delivery;
-    const response = await fetch(url, {
-      method: 'PATCH',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    })
-    return response;
+    try{
+      const response = await fetch(url, {
+        method: 'PATCH',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      })
+      return response;
+    }catch{
+      const response = await fetch(urlBackUp, {
+        method: 'PATCH',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      })
+      return response;
+    }
+    
   }
 }
