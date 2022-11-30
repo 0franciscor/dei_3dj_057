48b4d8fe7af3da270cfa7c45ae4dc8832b0ed21d
manu-07-ela <114668234+manu-07-ela@users.noreply.github.com>
2022-11-30 15:39:35 +0000

    #85 - Correção dos fatos dinamicos

--- ALGAV/US1.pl
+++ ALGAV/US1.pl
@@ -4,6 +4,8 @@
 
 :-dynamic infoTime/2.
 
+infoTime(100000000000000000,_).
+
 allPaths(L, LF) :-((checkIfCityExist(L),!,findMatosinhos(M1),deleteMatosinhos(M1,L,L2),findall(LAux,permutation(L2,LAux),L3),appendMatosinhos([M1],L3,LF)); write('One of the entered coordinates does not correspond to a warehouse that is in the system.')).
 
 deleteMatosinhos(A,L,L1):- ((member(A,L),!,delete(L,A,L1)); L1=L).
@@ -97,14 +99,9 @@ checkIfCharges(IDTRUCK,DL,BW,[H|_],TRUCKE,FENERGY,CHARGETIME):- energy(IDTRUCK,B
 %compares best time of all paths
 
 comparePaths([],_,_):-!.
-comparePaths([H|T],IDTRUCK,DL):-comparePaths(T,IDTRUCK,DL),carateristicasCam(IDTRUCK,_,_,BAT,_,_), TOTALTIME is 0,analisePath(H,IDTRUCK,DL,BAT,TOTALTIME,TIME),
-    createPathsWithEnergy(H,TIME).
-
-createPathsWithEnergy(H,T):-assert(infoTime(H,T)).
-
-getAllInfoTimes(infoTime(L,T)):- setof(T,infoTime(L,T),[T|_]).
+comparePaths([H|T],IDTRUCK,DL):-comparePaths(T,IDTRUCK,DL),carateristicasCam(IDTRUCK,_,_,BAT,_,_), TOTALTIME is 0,analisePath(H,IDTRUCK,DL,BAT,TOTALTIME,TIME), infoTime(TAux, _), ((TAux > TIME,!, retract(infoTime(TAux,_)),assert(infoTime(TIME,H))); true).
 
 appendDelivery(L,L1):- append([1], L, L2), append(L2,[1],L1).
 
 %append to list
-quickestPath(IDTRUCK,DELL,T):-appendDelivery(DELL,DL),findAllPaths(DL,AP), comparePaths(AP,IDTRUCK,DL),getAllInfoTimes(infoTime(T,_)).
+quickestPath(IDTRUCK,DELL,L,T):-retract(infoTime(_,_)),assert(infoTime(100000,_)),appendDelivery(DELL,DL),findAllPaths(DL,AP), comparePaths(AP,IDTRUCK,DL),!,infoTime(T,L).
