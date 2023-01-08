3f92a38e054abe44f9c3ff7b0c30744544b38f20
NunoBarbosa20 <1210822@isep.ipp.pt>
2023-01-08 21:41:50 +0000

    #186 e2e logistics

--- UI/cypress/e2e/logisticsManager.cy.ts
+++ UI/cypress/e2e/logisticsManager.cy.ts
@@ -1,4 +1,47 @@
 describe('Logistics Manager Tab', () => {
+
+    beforeEach(()=>{
+        cy.visit('http://localhost:4200/Login');
+        cy.wait(1000);
+        cy.get('input[formcontrolname="email"]').type('log.manager@gmail.com');
+
+        cy.get('input[formcontrolname="password"]').type('Logistics123');
+
+        cy.get('label').contains('I agree with the terms and conditions').find('span').click();
+
+        cy.get('button').contains('Log In').click();
+
+        cy.wait(2000);
+
+    })
+
+    /* after(() => {
+        //get mat-icon named logout and click 
+        cy.get('mat-icon').contains('logout').click();
+        cy.visit('http://localhost:4200/Login');
+        cy.wait(1000);
+
+        cy.get('input[formcontrolname="password"]').type('CappuJSON');
+
+        cy.get('label').contains('I agree with the terms and conditions').find('span').click();
+
+        
+        cy.get('button').contains('Log In').click();
+        cy.wait(1000);
+        cy.get('button').contains('Logistics Manager').click();
+        cy.wait(1000);
+
+        cy.get('div').contains('Create Path').should('be.visible');
+        cy.get('div').contains('Search').should('be.visible');
+        cy.get('mat-form-field').contains('Starting Warehouse').should('be.visible');
+        cy.get('mat-form-field').contains('Destination Warehouse').should('be.visible');
+        cy.get('div').contains('See Road Network').should('be.visible');
+        cy.get('div').contains('Truck planning').should('be.visible');
+
+
+    }) */
+
+
     it('should display the Logistics manager menu', () => {
         cy.visit('http://localhost:4200/Logistics/Home/LogisticsManager');
 
@@ -22,23 +65,14 @@ describe('Logistics Manager Tab', () => {
         cy.get('mat-card-subtitle').contains('Manager of Logic').should('be.visible');
     })
 
-    it('should display the Path list', () => {
-        cy.get('input[formControlName="startWHId"]').type('WH1');
-        cy.get('button').contains('Search').click();
-        cy.get('th').contains('Start Warehouse').should('be.visible');
-        cy.get('th').contains('Destination warehouse').should('be.visible');
-        cy.get('th').contains('Path distance').should('be.visible');
-        cy.get('th').contains('Travel time').should('be.visible');
-        cy.get('th').contains('Wasted energy').should('be.visible');
-        cy.get('th').contains('Extra Time').should('be.visible');
-    })
-
     it('should redirect to the path creation page', () => {
         cy.get('button').contains('Create Path').click();
         cy.url().should('include', '/Logistics/Path/CreatePath');
     })
 
     it('should display the path creation page', () => {
+        cy.get('button').contains('Create Path').click();
+        cy.url().should('include', '/Logistics/Path/CreatePath');
         cy.get('mat-card-title').contains('Create Path').should('be.visible');
         cy.get('form').contains('Start Warehouse ID').should('be.visible');
         cy.get('form').contains('Destination Warehouse ID').should('be.visible');
@@ -49,6 +83,8 @@ describe('Logistics Manager Tab', () => {
     })
 
     it('should create the path creation page', () => {
+        cy.get('button').contains('Create Path').click();
+        cy.url().should('include', '/Logistics/Path/CreatePath');
         cy.get('input[formControlName="startWHId"]').type('t1');
         cy.get('input[formControlName="destinationWHId"]').type('t2');
         cy.get('input[formControlName="pathDistance"]').type('1000');
@@ -58,11 +94,20 @@ describe('Logistics Manager Tab', () => {
 
         cy.get('button').contains('Create').click();
 
-        cy.wait(1000);
-
     })
 
     it('should display create dialog box', () => {
+        cy.get('button').contains('Create Path').click();
+        cy.url().should('include', '/Logistics/Path/CreatePath');
+        cy.get('input[formControlName="startWHId"]').type('t1');
+        cy.get('input[formControlName="destinationWHId"]').type('t2');
+        cy.get('input[formControlName="pathDistance"]').type('1000');
+        cy.get('input[formControlName="pathTravelTime"]').type('1000');
+        cy.get('input[formControlName="wastedEnergy"]').type('1000');
+        cy.get('input[formControlName="extraTravelTime"]').type('1000');
+
+        cy.get('button').contains('Create').click();
+
         cy.get('p').contains('Error creating Path').should('be.visible');
         cy.get('button').contains('Ok').click();
     })
@@ -77,6 +122,8 @@ describe('Logistics Manager Tab', () => {
     })
 
     it('should display the 4 available options ', ()=>{
+        cy.get('button').contains('Truck planning').click();
+        cy.url().should('include', '/Logistics/TruckPlanning');
         cy.get('mat-form-field').contains('Truck').should('be.visible');
         cy.get('mat-form-field').contains('Choose a date').should('be.visible');
         cy.get('div').contains('Find best path').should('be.visible');
